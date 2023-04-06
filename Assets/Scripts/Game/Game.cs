53436b709719036594b0fa798c934533b16672c3
Doomky <james.richet@epita.fr>
2023-04-06 20:22:12 +0200

    [*] Add Game.cs

---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,34 @@
+﻿using Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Game : Game<Game.StateMachine, Game.Action>
+    {
+        public enum Action
+        {
+            
+        }
+
+        public new class StateMachine : GameStateMachine<Action>
+        {
+            public StateMachine() : base(null)
+            {
+            }
+
+            protected override bool TryGetNextState(Action action, out GameState nextState)
+            {
+                switch (action)
+                {
+                    default:
+                        nextState = null;
+                        return false;
+                }
+            }
+        }
+    }
+}
