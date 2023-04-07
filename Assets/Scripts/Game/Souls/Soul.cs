f48ca8a735b737a2276791ba9be3d3e8e9358de6
Brice Cornuau <daxter5onyx@gmail.com>
2023-04-07 21:55:35 +0200

    [*] Soul

--- Assets/Scripts/Game/Souls/Soul.cs
+++ Assets/Scripts/Game/Souls/Soul.cs
@@ -1,20 +1,61 @@
 ﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace Game
 {
-    public abstract class Soul : SerializedScriptableObject, ISoul
+    [CreateAssetMenu(menuName = "New/Soul", fileName = "Soul", order = 0)]
+    public class Soul : SerializedScriptableObject, ISoul
     {
-        IPrimaryFragment ISoul.PrimaryFragement { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        ISecondaryFragment ISoul.SecondaryFragement { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        [SerializeField] private IPrimaryFragment _primaryFragment;
+        [SerializeField] private ISecondaryFragment _secondaryFragment;
 
-        void ISoul.Bind(IPlayer player, bool isPrimary)
+        public IPrimaryFragment PrimaryFragment
         {
-            throw new System.NotImplementedException();
+            get => _primaryFragment;
+            set => _primaryFragment = value;
         }
 
-        void ISoul.Unbind()
+        public ISecondaryFragment SecondaryFragment
         {
-            throw new System.NotImplementedException();
+            get => _secondaryFragment;
+            set => _secondaryFragment = value;
+        }
+
+        public void Bind(IPlayer player, bool isPrimary)
+        {
+            if (isPrimary)
+            {
+                Bind(player, PrimaryFragment);
+                Bind(player, SecondaryFragment);
+            }
+            else
+            {
+                Bind(player, SecondaryFragment);
+            }
+        }
+
+        private void Bind(IPlayer player, ISecondaryFragment fragment)
+        {
+            player.OnFire        += fragment.OnFire;
+            player.OnHit         += fragment.OnHit;
+            player.OnMove        += fragment.OnMove;
+            player.OnFixedUpdate += fragment.OnFixedUpdate;
+            player.OnTakeDamage  += fragment.OnTakeDamage;
+        }
+
+        void ISoul.Unbind(IPlayer player)
+        {
+            player.OnFire        -= PrimaryFragment.OnFire;
+            player.OnHit         -= PrimaryFragment.OnHit;
+            player.OnMove        -= PrimaryFragment.OnMove;
+            player.OnFixedUpdate -= PrimaryFragment.OnFixedUpdate;
+            player.OnTakeDamage  -= PrimaryFragment.OnTakeDamage;
+            
+            player.OnFire        -= SecondaryFragment.OnFire;
+            player.OnHit         -= SecondaryFragment.OnHit;
+            player.OnMove        -= SecondaryFragment.OnMove;
+            player.OnFixedUpdate -= SecondaryFragment.OnFixedUpdate;
+            player.OnTakeDamage  -= SecondaryFragment.OnTakeDamage;
         }
     }
 }
